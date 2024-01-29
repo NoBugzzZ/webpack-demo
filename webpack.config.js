b6a912f2f8121a76b38535a416a1f711208b5953
zheng.tang <zheng.tang@goodark.com>
2024-01-29 10:39:50 +0800

    dev-company first push

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const { mode } = require('webpack-nano/argv')
 const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin')
+const { WebpackPluginServe } = require('webpack-plugin-serve')
+
+console.log(mode, ' ', process.env.PORT, ' ', process.env.port)
 
 module.exports = {
-    mode,
-    plugins: [
-        new MiniHtmlWebpackPlugin({
-            context: {
-                title: 'webpack-demoooo~'
-            }
-        })
-    ]
+  watch: mode === 'development',
+  entry: ['./src', 'webpack-plugin-serve/client'],
+  mode,
+  plugins: [
+    new MiniHtmlWebpackPlugin({
+      context: {
+        title: 'webpack-demoooo~'
+      }
+    }),
+    new WebpackPluginServe({
+      port: parseInt(process.env.PORT, 10) || 8080,
+      static: './dist',
+      liveReload: true,
+      waitForBuild: true,
+      host:'127.0.0.1'
+    })
+  ]
 }
