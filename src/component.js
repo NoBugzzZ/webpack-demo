c0771743c0dccb715c20c7f1b7ebc60396d120f1
NoBugzzZ <739651338@qq.com>
2024-02-18 13:19:28 +0800

    Loading Assets: Loading JavaScript

--- src/component.js
+++ src/component.js
@@ -1,6 +1,20 @@
 import src from './业务综合分析.png'
 
+async function test(t = '113') {
+  console.log(t, '1')
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('end!!')
+    }, 1000)
+  }).then((res) => {
+    console.log(res)
+  })
+  console.log('2')
+}
+
 export default (text = 'hello webpack') => {
+  test()
+
   console.log(text)
   const el = document.createElement('div')
   el.className = 'rounded bg-red-100 border max-w-md m-4 p-4'
@@ -10,11 +24,11 @@ export default (text = 'hello webpack') => {
   p.className = 'p'
   p.innerHTML = 'svg'
   el.appendChild(p)
-  
-  const img=document.createElement('img')
-  img.src=src
+
+  const img = document.createElement('img')
+  img.src = src
   el.appendChild(img)
-  
+
   const font = document.createElement('p')
   font.className = 'font'
   font.innerHTML = 'hello'
